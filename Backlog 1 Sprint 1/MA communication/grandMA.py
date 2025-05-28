d3eb72fa616bd805366825bfff7ebb510848b18a
timsjt <tim.jtsim@gmail.com>
2025-05-28 14:46:51 +0800

    Create grandMA.py

---
+++ Backlog 1 Sprint 1/MA communication/grandMA.py
@@ -0,0 +1,49 @@
+from pythonosc import udp_client, osc_message_builder
+import time
+
+def send_message(receiver_ip, receiver_port, address, message):
+	try:
+		# Create an OSC client to send messages
+		client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
+
+		# Send an OSC message to the receiver
+		client.send_message(address, message)
+
+		print("Message sent successfully.")
+	except:
+		print("Message not sent")
+
+
+#change the below values
+if __name__ == "__main__":
+    LAPTOP_IP = "192.168.8.185"		# send to laptop w grandMA3
+    PORT = 1025                     # laptop w grandMA3 port number
+    addr = "/gma3/cmd"
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Sequence 1")
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Cue 1")
+    time.sleep(1)
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Cue 2")
+    time.sleep(1)
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Cue 3")
+    time.sleep(1)
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Cue 4")
+    time.sleep(1)
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Cue 1")
+    time.sleep(1)
+	
+
+    send_message(LAPTOP_IP, PORT, addr, "Go Sequence 2")
+
+    send_message(LAPTOP_IP, PORT, addr, "Go+ Sequence 2")
+    time.sleep(1)
+
+    send_message(LAPTOP_IP, PORT, addr, "Go+ Sequence 2")
+    time.sleep(1)
+
+    send_message(LAPTOP_IP, PORT, addr, "Go+ Sequence 2")
